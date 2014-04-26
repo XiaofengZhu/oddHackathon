f00bd0011ba60ae579d2f231fe5ae2828fe74a8c
XiaofengZhu <zhuxiaofengwww@gmail.com>
2014-04-25 21:53:35 -0500

    link pages

--- hackathon/urls.py
+++ hackathon/urls.py
@@ -1,12 +1,27 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+# admin.autodiscover()
+
+# urlpatterns = patterns('',
+#     # Examples:
+#     # url(r'^$', 'hackathon.views.home', name='home'),
+#     # url(r'^blog/', include('blog.urls')),
+
+#     url(r'^admin/', include(admin.site.urls)),
+# )
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'hackathon.views.home', name='home'),
+    # url(r'^$', 'Sourcefinder.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', views.IndexView.as_view(), name="homepage"),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'Sourcerous/', include('findSource.urls', namespace = 'findSource')),
 )
+
+urlpatterns += patterns('django.contrib.staticfiles.views',
+        url(r'^static/(?P<path>.*)$', 'serve'),
+    )
