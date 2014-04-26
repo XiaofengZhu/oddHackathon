6960fcfe0ae8918db76dc8b1a650e8d2e9058d73
XiaofengZhu <zhuxiaofengwww@gmail.com>
2014-04-25 22:00:54 -0500

    fix error

--- hackathon/urls.py
+++ hackathon/urls.py
@@ -1,15 +1,9 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-# admin.autodiscover()
 
-# urlpatterns = patterns('',
-#     # Examples:
-#     # url(r'^$', 'hackathon.views.home', name='home'),
-#     # url(r'^blog/', include('blog.urls')),
+from hackathon import views
 
-#     url(r'^admin/', include(admin.site.urls)),
-# )
 admin.autodiscover()
 
 urlpatterns = patterns('',
