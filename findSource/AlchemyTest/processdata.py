193d46a203b81e944a7558d4e5fdef8a7ab93633
XiaofengZhu <zhuxiaofengwww@gmail.com>
2014-04-26 00:49:41 -0500

    add data

--- findSource/AlchemyTest/processdata.py
+++ findSource/AlchemyTest/processdata.py
@@ -3,24 +3,24 @@ import sys, json
 def readJson():
 
 	#return list
-	output = {}
+	output = []
 
 	f = open('/static/findSource/data/termDetails.txt','r')
 	jsonObj = json.loads(f.read())
 
-	# for obj in jsonObj:  
-	#     termDict={}
-	#     termDict['id']=obj['id']
-	#     termDict['year']=obj['year']
-	#     termDict['quarter']=obj['quarter']
+	for obj in jsonObj:  
+	    termDict={}
+	    termDict['id']=obj['id']
+	    termDict['year']=obj['year']
+	    termDict['quarter']=obj['quarter']
 
-	#     output.append(termDict)
-	years = list(set([i['year']for i in jsonObj])) #[2005,2007,....]
-	output['years'] = years
+	    output.append(termDict)
+	# years = list(set([i['year']for i in jsonObj])) #[2005,2007,....]
+	# output['years'] = years
 
-	terms= list(set([i['quater']for i in jsonObj]))
-	output['quaters'] = quaters
+	# terms= list(set([i['quater']for i in jsonObj]))
+	# output['quaters'] = quaters
 
 	f.close()
 
-	return years
+	return output
