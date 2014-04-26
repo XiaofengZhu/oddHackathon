593a428b4c010c972b500b69a8662a782f50ce26
XiaofengZhu <zhuxiaofengwww@gmail.com>
2014-04-25 19:51:50 -0500

    push an odd code do not pull it

--- hackathon/settings/__init__.py
+++ hackathon/settings/__init__.py
@@ -1,5 +1,5 @@
 """
-Django settings for hackathon project.
+Django settings for Sourcefinder project.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/topics/settings/
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from django.conf import global_settings
+
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
@@ -29,6 +31,11 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+TEMPLATE_DIRS = (
+        'templates',
+        'findSource/templates/'
+)
+
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
@@ -36,6 +43,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'findSource',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -49,18 +57,12 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'hackathon.urls'
 
-WSGI_APPLICATION = 'hackathon.wsgi.application'
+WSGI_APPLICATION = 'hackathon.wsgi.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
@@ -79,4 +81,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_PATH, '../../staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+        os.path.join(PROJECT_PATH, '../../findSource/static'),
+        #'/findSource/static/',
+)
+
+
+TEMPLATE_LOADERS = (
+        'djmako.MakoLoader',
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+)
+
+MAKO_TEMPLATE_DIRS = TEMPLATE_DIRS
---
+++ hackathon/settings/dev_local.py
@@ -0,0 +1,12 @@
+from Sourcefinder.settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db',
+        'USER': 'db_user',
+        'PASSWORD': 'db_pwd',
+        'HOST': '127.0.0.1',
+        'PORT': '',
+    }
+}
