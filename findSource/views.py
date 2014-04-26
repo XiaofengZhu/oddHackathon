430e53d226f1764d6d39226025cc6c2bc5be03ed
XiaofengZhu <zhuxiaofengwww@gmail.com>
2014-04-26 18:12:11 -0500

    k

--- findSource/views.py
+++ findSource/views.py
@@ -46,8 +46,8 @@ class ResultView(TemplateView):
 
         graph=getGraph(term_id,key_word)
         context['graph']=graph
-        f = open('asia.json','w')
-        context['graphpath']=os.path.abspath('asia.json')        
+        f = open('maps/asia.json','w')
+        context['graphpath']=os.path.abspath(f)        
         f.write(json.dumps(graph))
         f.close()
 
